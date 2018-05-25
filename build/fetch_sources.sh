61435a626a55c405161270ee0ae7272ad100287f
jaeyoung.choi <lactrious@gmail.com>
2018-05-25 15:11:20 +0900

    Change ffmpeg to 3.3.7

--- build/fetch_sources.sh
+++ build/fetch_sources.sh
@@ -1,4 +1,10 @@
-wget http://ffmpeg.org/releases/ffmpeg-3.1.3.tar.bz2
-tar -xvf ffmpeg-3.1.3.tar.bz2
-mv ffmpeg-3.1.3 ffmpeg
-rm ffmpeg-3.1.3.tar.bz2
+#!/bin/bash
+
+VERSION=3.3.7
+
+rm -rf ffmpeg
+wget http://ffmpeg.org/releases/ffmpeg-${VERSION}.tar.bz2
+tar -xvf ffmpeg-${VERSION}.tar.bz2
+mv ffmpeg-${VERSION} ffmpeg
+rm ffmpeg-${VERSION}.tar.bz2
+
