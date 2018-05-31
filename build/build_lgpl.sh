2af67bbf39f0b152f4386a5d0aa2fdd359086cb8
jaeyoung.choi <lacti@voyagerx.com>
2018-05-31 19:04:24 +0900

    Build with 3.4.2

--- build/build_lgpl.sh
+++ build/build_lgpl.sh
@@ -20,11 +20,12 @@ make clean
 
 CPPFLAGS="-D_XOPEN_SOURCE=600" emconfigure ./configure --cc="emcc" --prefix=$(pwd)/../dist --enable-cross-compile --target-os=none --arch=x86_64 --cpu=generic \
     --disable-ffplay --disable-ffprobe --disable-ffserver --disable-asm --disable-doc --disable-devices --disable-pthreads --disable-w32threads --disable-network \
-    --disable-hwaccels --disable-parsers --disable-bsfs --disable-debug --disable-protocols --disable-indevs --disable-outdevs --enable-protocol=file
+    --disable-hwaccels --disable-parsers --disable-bsfs --disable-debug --disable-protocols --disable-indevs --disable-outdevs --enable-protocol=file \
+    --nm=$(which llvm-nm) --disable-stripping
 
 # Because there doesn't appear to be a way to tell configure that arc4random isn't actually there
 sed -i.bak -e 's/#define HAVE_ARC4RANDOM 1/#define HAVE_ARC4RANDOM 0/' ./config.h
-sed -i.bak -e 's/HAVE_ARC4RANDOM=yes/HAVE_ARC4RANDOM=no/' ./config.mak
+sed -i.bak -e 's/HAVE_ARC4RANDOM=yes/HAVE_ARC4RANDOM=no/' ./ffbuild/config.mak
 
 make -j
 make install
@@ -39,7 +40,7 @@ rm *.bc
 cp lib/libz.a libz.bc
 cp ../ffmpeg/ffmpeg ffmpeg.bc
 
-emcc -v -s TOTAL_MEMORY=536870912 -Os ffmpeg.bc libz.bc -o ../ffmpeg.js --pre-js ../ffmpeg_pre.js --post-js ../ffmpeg_post.js -s ALLOW_MEMORY_GROWTH=1
+emcc -v -s TOTAL_MEMORY=1073741824 -O3 ffmpeg.bc libz.bc -o ../ffmpeg.js --pre-js ../ffmpeg_pre.js --post-js ../ffmpeg_post.js
 
 cd ..
 
